d653f7daf67433c41a4aec73d17c175df5a7ed14
burgerhex <aviv.shai@gmail.com>
2021-10-09 03:32:43 -0700

    Attempt first server connection

--- src/client/main.py
+++ src/client/main.py
@@ -1,2 +1,28 @@
 import grovepi
+import socket
+import time
+import threading
 
+ROTARY_PORT = 2  # port A2
+SLEEP_TIME = 1
+
+# noinspection SpellCheckingInspection
+SERVER_HOST = "Avivs-MBP-2.mshome.net"
+SERVER_PORT = 57944
+
+lock = threading.Lock()
+
+sock = socket.socket()
+sock.connect((SERVER_HOST, SERVER_PORT))
+
+while True:
+    msg = sock.recv(1024)
+    if msg:
+        print("received: " + msg.decode())
+        sock.send("hello server! from rpi".encode())
+
+    with lock:
+        rotary_reading = grovepi.analogRead(ROTARY_PORT)
+        print("rotary reading: " + rotary_reading)
+
+    time.sleep(SLEEP_TIME)
--- src/main/java/io/github/burgerhex/rpiplugin/RPiPlugin.java
+++ src/main/java/io/github/burgerhex/rpiplugin/RPiPlugin.java
@@ -2,16 +2,61 @@ package io.github.burgerhex.rpiplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+//@SuppressWarnings("unused")
 public final class RPiPlugin extends JavaPlugin {
+
+    public static final int PORT = 57944;
+
+    private ServerSocket serverSocket;
+    private Socket socket;
+
+    private PrintWriter out;
+    private BufferedReader in;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getLogger().info("Starting server...");
+
+        try {
+            // TODO: start a new thread with this, since this is
+            //  blocking and probably shouldn't be done in onEnable()
+            serverSocket = new ServerSocket(PORT);
+            getLogger().info("Waiting for connection from RPi...");
+            socket = serverSocket.accept();
+            getLogger().info("Got a connection!");
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            out.println("Hello RPi");
+
+            String line = in.readLine();
+            while (line == null)
+                line = in.readLine();
+
+            getLogger().info("Received this from RPi: " + line);
+        } catch (IOException e) {
+            getLogger().warning("Couldn't start server and get connection; aborting!");
+//            return;
+        }
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        try {
+            if (serverSocket != null)
+                serverSocket.close();
+            if (socket != null)
+                socket.close();
+        } catch (IOException e) {
+            getLogger().warning("Couldn't close sockets!");
+        }
     }
 }
